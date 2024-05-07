9b3b4a4d7436e5b0378bdacef185e808ff5bcb9e
kmdshojib <kmd.shojib@gmail.com>
2024-05-07 22:58:08 +0600

    learning about difference void

--- index.cpp
+++ index.cpp
@@ -1,7 +1,26 @@
 #include <iostream>;
 using namespace std;
 
-int main(){
-    cout << "Hello, world!";
+typedef struct employee
+{
+    int eid;
+    string fullName;
+    float salary;
+};
+void employeeFunc()
+{
+    employee rafi;
+    rafi.eid = 20106;
+    rafi.fullName = "Rafi Hasan";
+    rafi.salary = 308980;
+
+    cout << "Employee Details : "
+         << "\n"
+         << "Name: " << rafi.fullName << "\n"
+         << "Salary: " << rafi.salary;
+}
+int main()
+{
+    employeeFunc();
     return 0;
 }
